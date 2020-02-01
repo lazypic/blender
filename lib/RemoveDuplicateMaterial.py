b5175f864dac67dde35480e7ae7d70f71d954892
YoonJieun <thatsy@naver.com>
2020-02-01 17:53:52 +0900

    중복쉐이더 지우는 스크립트 추가 removeDuplicateMaterial

---
+++ lib/RemoveDuplicateMaterial.py
@@ -0,0 +1,25 @@
+import bpy
+
+'''
+replace material with 3 characters at the end to material without number 
+ex) 'material.001' --> 'material' 
+    'material.099' --> 'material'
+'''
+
+#make all material list 
+mat_list = [mat.name for mat in bpy.data.materials]
+
+#remove duplicate material name
+for obj in bpy.context.scene.objects:
+    for objmat in obj.material_slots:
+        if objmat.material.name[-3:].isnumeric():
+            # last 3 character are numbers 
+            if objmat.material.name[:-4] in mat_list:
+                #material without number exist
+                objmat.material=bpy.data.materials[objmat.material.name[:-4]]
+                #replace duplicated material to orign material 
+                
+#remove unused matrials
+for material in bpy.data.materials:
+    if not material.users:
+        bpy.data.materials.remove(material)
